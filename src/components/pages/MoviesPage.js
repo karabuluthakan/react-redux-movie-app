792a11d852f2e25599cdb3a5de93626857b3373a
Hakan Karabulut <hakan.karabulut@ugurbilgi.net>
2018-07-03 11:11:36 +0300

    movies components
    movies list and movies page component first step completed

--- src/components/pages/MoviesPage.js
+++ src/components/pages/MoviesPage.js
@@ -1,13 +1,28 @@
-import React, {Component} from 'react'; 
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import MoviesList from '../MoviesList';
 
 class MoviesPage extends Component {
+    static propTypes = {
+        movies: PropTypes.array.isRequired
+    };
+
     render() {
         return (
             <div>
                 <h2>Movies Page</h2>
+                <MoviesList movies={this.props.movies}></MoviesList>
             </div>
         );
     }
 }
 
-export default MoviesPage;
+const mapStateToProps = ( { movies } ) => {
+    return{
+        movies
+    }
+}
+
+export default connect( mapStateToProps )(MoviesPage);
