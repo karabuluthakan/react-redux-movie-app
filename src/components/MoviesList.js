a4f587b927e73fef196e5ffca504628f2e8ef9f8
Hakan Karabulut <hakan.karabulut@ugurbilgi.net>
2018-07-04 16:01:35 +0300

    movies listing pages

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+import {Grid} from 'semantic-ui-react';
 
 const MoviesList = ({movies}) => {
     const emptyMessage = (
@@ -9,7 +11,19 @@ const MoviesList = ({movies}) => {
     const moviesList = (
         <div>
             {
-               movies.error.response ?  <h3>Error retrieving data!</h3> : 'Hata yok'
+               movies.error.response
+                   ?  <h3>Error retrieving data!</h3>
+                   :
+                   <Grid stackable columns={3}>
+                       {
+                           movies.movies.map(
+                               movie => <MovieCard
+                                   movie={movie}
+                                   key={movie._id}>
+                                   {movie.title}>
+                               </MovieCard>)
+                       }
+                   </Grid>
             }
         </div>
     );
